74d27b65daef148e19e53d47fc6d6acabec6026c
superphs333 <superphs333@naver.com>
2021-02-18 10:59:27 +0000

    비밀번호 변경end

--- get_member_info.php
+++ get_member_info.php
@@ -1,10 +1,21 @@
 <?php
+/*
+회원의 유일값을 받아오고, 
+요청한 정보를 echo
+*/
 error_reporting(E_ALL); // 에러 표시
 ini_set('display_errors',1);
     // php 설정 변경
 // 데이터베이스 관련 파일 추가
 include_once('db_config.php');
 
-// 회원정보를 가져올 수 있는 값 받아오기
-$login_value = $_POST['login_value'];
+// 값 받아오기
+$login_value = $_POST['login_value']; // 회원정보 유일값
+$sort = $_POST['sort']; // 가져올 정보
+
+$temp = "select {$sort} from members where login_value='{$login_value}'";
+$sql = mq($temp);
+$output = $sql->fetch_array();
+$result = $output[$sort];
+echo $result;
 ?>
