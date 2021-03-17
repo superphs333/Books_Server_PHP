c9d481a6841ff56a11797200c86382e76856b841
superphs333 <superphs333@naver.com>
2021-03-17 23:03:18 +0000

    리뷰쓰기

--- Data/Get_My_Book.php
+++ Data/Get_My_Book.php
@@ -9,7 +9,7 @@ $unique_book_value = $_POST['unique_book_value'];
 
 
 // 해당 데이터 
-$temp = "SELECT Books.unique_book_value, title, authors, thumbnail, contents, My_Books.from_, isbn, status, rating, publisher FROM My_Books JOIN Books ON My_Books.unique_book_value=Books.unique_book_value WHERE Books.unique_book_value='{$unique_book_value}'";
+$temp = "SELECT Books.unique_book_value, title, authors, thumbnail, contents, My_Books.from_, isbn, status, rating, publisher, review FROM My_Books JOIN Books ON My_Books.unique_book_value=Books.unique_book_value WHERE Books.unique_book_value='{$unique_book_value}'";
 $sql = mq($temp);
 $dbdata = array();
 while($row=$sql->fetch_assoc()){
