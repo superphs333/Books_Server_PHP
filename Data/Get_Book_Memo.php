18cc8d45edd800a8b4bcb10767ee85df3995fa2e
superphs333 <superphs333@naver.com>
2021-03-24 07:23:46 +0000

    update 책메모 리스트

--- Data/Get_Book_Memo.php
+++ Data/Get_Book_Memo.php
@@ -9,25 +9,70 @@ $unique_book_value = $_POST['unique_book_value'];
 $login_value = $_POST['login_value'];
 
 // 임시
-$unique_book_value = '210316084357temp_book';
-$login_value = 'dlthdus943@naver.com';
+// $unique_book_value = '210316084357temp_book';
+// $login_value = 'dlthdus943@naver.com';
 
 // 객체 => Book_Memo
-// class Book_Memo{
-//     public $idx, $login_value, $nickanme, $profile_url, $unique_book_value, $title, $thumbnail, $date_time, $img_urls, $memo, $page, $open;
-// }
+class Book_Memo{
+    public $idx, $login_value, $nickname, $profile_url, $unique_book_value, $title, $thumbnail, $date_time, $img_urls, $memo, $page, $open, $count_heart, $count_comment,$check_heart;
+}
+    // check_heart = 내가 하트를 눌렀는지 체크(눌렀으면 1, 누르지 않았으면 0)
+$list = array();
 
 // 해당 데이터 
 $temp = "SELECT Book_Memo.idx, Book_Memo.login_value, members.nickname, members.profile_url, Book_Memo.unique_book_value, Books.title, Books.thumbnail, Book_Memo.date_time,img_urls, Book_Memo.memo, Book_Memo.page, Book_Memo.open, Book_Memo.count_heart, Book_Memo.count_comment From Book_Memo JOIN members ON Book_Memo.login_value=members.login_value JOIN Books ON Books.unique_book_value=Book_Memo.unique_book_value WHERE Books.unique_book_value='{$unique_book_value}' AND members.login_value='{$login_value}'";
 $sql = mq($temp);
-if($sql){
-    //echo "su";
-    $dbdata = array();
-    while($row=$sql->fetch_assoc()){
-        $dbdata[]=$row;
+
+while($row=$sql->fetch_array()){
+    // 객체생성
+    $data = new Book_Memo();
+
+    /*
+    값 대입하기
+    */
+    $data->idx=$row['idx'];
+    $data->login_valuedx=$row['login_value'];
+    $data->nickname=$row['nickname'];
+    $data->profile_url=$row['profile_url'];
+    $data->unique_book_value=$row['unique_book_value'];
+    $data->title=$row['title'];
+    $data->thumbnail=$row['thumbnail'];
+    $data->date_time=$row['date_time'];
+    $data->img_urls=$row['img_urls'];
+    $data->memo=$row['memo'];
+    $data->page=$row['page'];
+    $data->open=$row['open'];
+    $data->count_heart=$row['count_heart'];
+    $data->count_comment=$row['count_comment'];
+    
+    // 하트체크 확인
+    $temp_heart = "select * from Heart_Memo where login_value='{$login_value}' and idx_memo={$row['idx']}";
+    $sql_heart = mq($temp_heart);
+    //echo $temp_heart;
+    $count = mysqli_num_rows($sql_heart);
+    if($count==0){
+        $data->check_heart=false;
+    }else{
+        $data->check_heart=true;
     }
-    echo json_encode($dbdata);
-}else{
-    echo mysqli_error($db);
+
+    // list에 대입하기
+    array_push($list,$data);
+
 }
+
+echo json_encode($list)
+
+
+
+// if($sql){
+//     //echo "su";
+//     $dbdata = array();
+//     while($row=$sql->fetch_assoc()){
+//         $dbdata[]=$row;
+//     }
+//     echo json_encode($dbdata);
+// }else{
+//     echo mysqli_error($db);
+// }
 ?>
